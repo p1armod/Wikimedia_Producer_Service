6a927b1cf6684b42c2bc66730fdfa8de5af92003
p1armod <parmodrns@gmail.com>
2025-12-20 10:58:23 +0530

    Implement WikimediaChangeProducer

---
+++ src/main/java/org/example/Producer/WikimediaChangeProducer.java
@@ -0,0 +1,55 @@
+package org.example.Producer;
+
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.MessageEvent;
+import com.launchdarkly.eventsource.EventHandler;
+import org.springframework.kafka.core.KafkaTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+import java.net.URI;
+
+@Component
+@RequiredArgsConstructor
+public class WikimediaChangeProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "wikimedia.raw";
+
+
+    public void start(){
+        EventHandler eventHandler = new EventHandler(){
+            @Override
+            public void onOpen() throws Exception {
+                System.out.println("Starting Wikimedia Change Producer");
+            }
+
+            @Override
+            public void onClosed() throws Exception {
+                System.out.println("Finished Wikimedia Change Producer");
+            }
+
+            @Override
+            public void onMessage(String event, MessageEvent messageEvent){
+                kafkaTemplate.send(TOPIC,messageEvent.getData().toString());
+            }
+
+            @Override
+            public void onComment(String s) throws Exception {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("Error in stream: " + t.getMessage());
+            }
+        };
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource eventSource =
+                new EventSource.Builder(eventHandler, URI.create(url)).build();
+
+        eventSource.start();
+    }
+}
